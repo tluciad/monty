bb583aea539af6038d8053a35dfe83c6bf3bbd0a
Mpqg <4534@holbertonschool.com>
2022-05-20 21:53:29 -0500

    fixing code

--- mod.c
+++ mod.c
@@ -8,20 +8,20 @@
  */
 void mod(stack_t **stack, unsigned int line_number)
 {
-	if (!s || !*s || !(*s)->next)
+	if (!stack || !*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
-		free_dlistint(*s);
+		free_dlistint(*stack);
 		exit(EXIT_FAILURE);
 	}
-	else if ((*s)->n == 0)
+	else if ((*stack)->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		((*s)->next)->n = ((*s)->next)->n % (*s)->n;
-		delete_dnodeint_at_index(s, 0);
+		((*stack)->next)->n = ((*stack)->next)->n % (*stack)->n;
+		delete_dnodeint_at_index(stack, 0);
 	}
 }
