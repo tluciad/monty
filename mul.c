849f5270e06b0dbc6ff4ce5f53c658adca4e7fe0
Mpqg <4534@holbertonschool.com>
2022-05-20 21:55:53 -0500

    fixing code

--- mul.c
+++ mul.c
@@ -8,15 +8,15 @@
  */
 void mul(stack_t **stack, unsigned int line_number)
 {
-	if (!s || !*s || !(*s)->next)
+	if (!stack || !*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
-		free_dlistint(*s);
+		free_dlistint(*stack);
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		((*s)->next)->n = ((*s)->next)->n * (*s)->n;
-		delete_dnodeint_at_index(s, 0);
+		((*stack)->next)->n = ((*stack)->next)->n * (*stack)->n;
+		delete_dnodeint_at_index(stack, 0);
 	}
 }
