c72922b7ea99c9b72611281cb3c4fec67d8b0893
tluciad <tluciad@gmail.com>
2022-05-22 21:29:48 -0500

    monty

---
+++ rotr.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+/**
+ * rotr - reverse the stack
+ *
+ * @s: head of the linked list
+ * @new_value: line number
+ * Return: no return
+ */
+void _rotr(stack_t **s, unsigned int new_value)
+{
+	stack_t *aux = NULL;
+	(void)new_value;
+
+	if (*s == NULL)
+		return;
+
+	if ((*s)->next == NULL)
+		return;
+
+	aux = *s;
+
+	for (; aux->next != NULL; aux = aux->next)
+		;
+
+	aux->prev->next = NULL;
+	aux->next = *s;
+	aux->prev = NULL;
+	(*s)->prev = aux;
+	*s = aux;
+}
